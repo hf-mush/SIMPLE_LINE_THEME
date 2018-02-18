8d9359067c5355246d18b10a26646a886a712197
fujitahiroshi <fuchantoshu@gmail.com>
2018-02-18 14:06:59 +0900

    modify: footer text

--- themes/simple-line/footer.php
+++ themes/simple-line/footer.php
@@ -2,7 +2,7 @@
     </div>
     <footer class="outer g-footer">
       <div class="inner g-footer__inner">
-        <div class="text copyright">&copy; 2017 - <? echo date('Y') . " " . bloginfo('name'); ?>.</div>
+        <div class="text copyright">&copy; 2017 - <?php echo date('Y'); ?> <? echo bloginfo('name'); ?>.</div>
       </div>
     </footer>
     <?php
